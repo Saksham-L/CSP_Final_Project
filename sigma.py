c720995892e2ce61acbbad48db7d42e26055c565
Saksham-L <70284118+Saksham-L@users.noreply.github.com>
2025-02-14 01:11:35 +0000

    Finished Fr this time

--- sigma.py
+++ sigma.py
@@ -4,12 +4,22 @@ import matplotlib.pyplot as plt
 
 df_lebron = pd.read_csv("lebron_career.csv")
 df_jordan = pd.read_csv("jordan_career.csv")
+print(df_lebron.head)
+
+print(df_jordan.head)
+total_pts_lebron = df_lebron["pts"].sum()
+total_pts_jordan = df_jordan["pts"].sum()
+print(f"Total Points - LeBron James: {total_pts_lebron}")
+print(f"Total Points - Michael Jordan: {total_pts_jordan}")
+total_tov_lebron = df_lebron["tov"].sum()
+total_tov_jordan = df_jordan["tov"].sum()
+print(f"Total Turnovers - LeBron James: {total_tov_lebron}")
+print(f"Total Turnovers - Michael Jordan: {total_tov_jordan}")
 def MakeAGraph(value, bins, name):
     B = int(bins)
     plt.figure(figsize=(10, 6))
     plt.hist(df_lebron[value], bins=B, alpha=0.5, label="LeBron James")
     plt.hist(df_jordan[value], bins=B, alpha=0.5, label="Michael Jordan")
-    
     plt.xlabel(f"{name} Per Game")
     plt.ylabel("Frequency")
     plt.legend()
